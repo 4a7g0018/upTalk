86dcf0a3a7fea123d452dd4dbf9637bba4120a55
xzz8868 <xzz8868@gmail.com>
2022-05-22 22:58:10 +0800

    feat:新增用房號刪除exist_room的Query

--- uptalk_spring/src/main/java/com/UPT/uptalk_spring/repository/IExistRoomRepository.java
+++ uptalk_spring/src/main/java/com/UPT/uptalk_spring/repository/IExistRoomRepository.java
@@ -2,6 +2,9 @@ package com.UPT.uptalk_spring.repository;
 
 import com.UPT.uptalk_spring.model.ExistRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -15,4 +18,9 @@ import java.util.Optional;
 public interface IExistRoomRepository extends JpaRepository<ExistRoom,Integer> {
 
     Optional<ExistRoom> findExistRoomByRoomNumber(Integer roomNumber);
+
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE FROM exist_room WHERE room_number=?1",nativeQuery = true)
+    void deleteRoomByRoomNumber(Integer roomNumber);
 }
